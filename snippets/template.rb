c4db6b4389a26abd7a75973b41fe77cc6f04d3f9
Tomas Korcak <korczis@gmail.com>
2014-05-18 01:09:53 -0700

    Template uses connecting using username and password

--- snippets/template.rb
+++ snippets/template.rb
@@ -2,21 +2,15 @@
 
 require 'gooddata'
 
-## add this to ~/.bash_profile with proper user, password, token, ...
-# export GD_GEM_USER="josef.novak@gooddata.com"
-# export GD_GEM_PASSWORD="xxx"
-# export GD_PROJECT_TOKEN=gooddataToken
-
-username = ENV['GD_GEM_USER']
-password = ENV['D_GEM_PASSWORD']
-# token = ENV['GD_PROJECT_TOKEN']
+username = 'svarovsky+gem_tester@gooddata.com'
+password = 'jindrisska'
 
 # Connect to platform using credentials
-puts "Connecting as #{username}"
+puts "Connecting as #{username} ..."
 GoodData.connect username, password
 
 # TODO: add code here
 
 # And disconnect finally
-puts 'Disconnecting'
+puts 'Disconnecting ...'
 GoodData.disconnect
