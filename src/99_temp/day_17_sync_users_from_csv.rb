5e97f8cd8bdda8679e84b1609e482fa10efd0bec
Tomas Korcak <korczis@gmail.com>
2014-08-23 18:16:58 -0700

    Invite, part of sync

---
+++ src/99_temp/day_17_sync_users_from_csv.rb
@@ -0,0 +1,19 @@
+# encoding: UTF-8
+
+require 'gooddata'
+
+PROJECT_ID = 'we1vvh4il93r0927r809i3agif50d7iz'
+
+GoodData.with_connection do |c|
+    project = c.project(PROJECT_ID)
+
+    path = File.join(File.dirname(__FILE__), '..', '..', 'data', 'users.csv')
+    puts "Loading #{path}"
+    CSV.foreach(path) do |row|
+        email = row[0]
+        role_name = row[1]
+        user = project.users.find { |user| user.email == email }
+        role = project.roles.find { |role| role.title == role_name }
+        project.set_user_roles(user, role)
+    end
+end
