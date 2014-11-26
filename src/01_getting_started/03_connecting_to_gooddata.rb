d86e00fd4a0cd7ccd5826691a958bf61033ffb9e
Tomas Korcak <korczis@gmail.com>
2014-11-26 16:58:20 +0100

    Fixed few typos, added old files

---
+++ src/01_getting_started/03_connecting_to_gooddata.rb
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+
+require 'gooddata'
+
+GoodData.with_connection('user', 'password') do |client|
+    # just so you believe us we are printing names of all the project under this account
+    client.projects.each do |p|
+        puts p.title
+    end
+end
