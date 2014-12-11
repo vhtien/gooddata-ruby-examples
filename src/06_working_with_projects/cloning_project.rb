febb105033b91c16fc8a5872fe69821f65e30594
Tomas Svarovsky <svarovsky@gooddata.com>
2014-12-11 13:40:28 -0800

    Added users and data params specifically to cloning.

--- src/06_working_with_projects/cloning_project.rb
+++ src/06_working_with_projects/cloning_project.rb
@@ -4,5 +4,8 @@ require 'gooddata'
 
 GoodData.with_connection('user', 'password') do |c|
   project = GoodData.use('project_pid')
-  cloned_project = project.clone(title: 'New title', auth_token: 'token')
+  cloned_project = project.clone(title: 'New title',
+                                 auth_token: 'token',
+                                 users: false,
+                                 data: ture)
 end
