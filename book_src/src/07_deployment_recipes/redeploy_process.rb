446358942d3c52ee5c87f00d11c1f04ecade2097
Tomas Svarovsky <svarovsky@gooddata.com>
2015-01-28 14:52:08 -0800

    FIxed a typo in the code template

--- book_src/src/07_deployment_recipes/redeploy_process.rb
+++ book_src/src/07_deployment_recipes/redeploy_process.rb
@@ -1,5 +1,3 @@
-[source,ruby]
-----
 # encoding: utf-8
 
 require 'gooddata'
@@ -8,4 +6,3 @@ client = GoodData.connect
 project = client.projects('project_id')
 process = project.processes.first
 process.deploy('path_to_your_process')
-----
