620c7870011e911fcf4b59d34514afbbb642b375
Tomas Svarovsky <svarovsky@gooddata.com>
2015-01-29 11:19:34 -0800

    Added a recipe

---
+++ book_src/src/11_working_with_data_filters/filters_from_columnar_csv.rb
@@ -0,0 +1,26 @@
+[source,ruby]
+----
+# encoding: utf-8
+
+require 'gooddata'
+
+client = GoodData.conect('login', 'password')
+project = client.projects('project_id')
+
+# First let's grab an attribute
+attribute = project.attributes('attr.region.city')
+
+# to set up a value we need a specific label
+# if the attribute has only one label, you can easily grab it by calling #primary_label
+label = attribute.primary_label
+
+# if a label has multiple labels, you can select the correct one like this
+# label = attribute.label_by_name('City name')
+
+filters = GoodData::UserFilterBuilder::get_filters('data.csv', { 
+  :type => :filter, 
+  :labels => [{:label => label, :column => 'company'}]
+})
+
+project.add_data_permissions(filters)
+----
