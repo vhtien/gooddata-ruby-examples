8d540af9848cc499aedcc8a9d836faf1252f38dd
Petr Cvengros <petr.cvengros@gooddata.com>
2015-05-22 13:49:15 +0200

    removed extra column in data in one page load example

--- book_src/src/06_working_with_projects/building_small_project_on_one_page.rb
+++ book_src/src/06_working_with_projects/building_small_project_on_one_page.rb
@@ -24,10 +24,10 @@ GoodData.with_connection('user', 'password') do |client|
   GoodData::with_project(project) do |p|
     # Load data
     commits_data = [
-      %w(lines_changed committed_on dev_id repo_id),
-      [1, '01/01/2014', 1, 1],
-      [3, '01/02/2014', 2, 2],
-      [5, '05/02/2014', 3, 1]]
+      %w(lines_changed committed_on dev_id),
+      [1, '01/01/2014', 1],
+      [3, '01/02/2014', 2],
+      [5, '05/02/2014', 3]]
     GoodData::Model.upload_data(commits_data, blueprint, 'commits')
 
     devs_data = [
