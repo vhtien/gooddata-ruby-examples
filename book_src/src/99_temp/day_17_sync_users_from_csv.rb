4972475f5025f308e0edb3d23fa9130f997aa571
Zdenek Svoboda <zsvoboda@gmail.com>
2015-06-01 17:12:47 +0200

    Chapters 1 and 2

--- book_src/src/99_temp/day_17_sync_users_from_csv.rb
+++ book_src/src/99_temp/day_17_sync_users_from_csv.rb
@@ -9,15 +9,15 @@ PROJECT_ID = 'we1vvh4il93r0927r809i3agif50d7iz'
 c = GoodData.connect('user', 'password')
 
 GoodData.with_connection do |c|
-    project = c.project(PROJECT_ID)
-
+  GoodData.with_project(PROJECT_ID) do |project|
     path = File.join(File.dirname(__FILE__), '..', '..', 'data', 'users.csv')
     puts "Loading #{path}"
     CSV.foreach(path) do |row|
-        email = row[0]
-        role_name = row[1]
-        user = project.users.find { |user| user.email == email }
-        role = project.roles.find { |role| role.title == role_name }
-        project.set_user_roles(user, role)
+      email = row[0]
+      role_name = row[1]
+      user = project.users.find { |user| user.email == email }
+      role = project.roles.find { |role| role.title == role_name }
+      project.set_user_roles(user, role)
     end
+  end
 end
