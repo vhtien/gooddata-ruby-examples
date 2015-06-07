bcd686df5137bcdb9445808a8ae9e74400ac2fb6
Zdenek Svoboda <zsvoboda@gmail.com>
2015-06-07 13:35:46 -0700

    Automation recipes

--- book_src/src/99_temp/day_02_metrics_to_csv.rb
+++ book_src/src/99_temp/day_02_metrics_to_csv.rb
@@ -5,9 +5,6 @@ require 'gooddata'
 # Project ID
 PROJECT_ID = 'we1vvh4il93r0927r809i3agif50d7iz'
 
-# Connect to GoodData platform
-c = GoodData.connect('user', 'password')
-
 GoodData.with_connection do |c|
   GoodData.with_project(PROJECT_ID) do |project|
     CSV.open(project.pid + "_metrics.csv", 'wb') do |csv|
--- book_src/src/99_temp/day_03_schedule_process.rb
+++ book_src/src/99_temp/day_03_schedule_process.rb
@@ -5,9 +5,6 @@ require 'gooddata'
 # Project ID
 PROJECT_ID = 'we1vvh4il93r0927r809i3agif50d7iz'
 
-# Connect to GoodData platform
-c = GoodData.connect('user', 'password')
-
 PROCESS_ID = 'dc143d80-58a1-4acd-96b6-8d11fc4571de'
 EXECUTABLE = './graph/graph.grf'
 
