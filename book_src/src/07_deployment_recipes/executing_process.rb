13623fc388bf8350599229b3721e520b09cf6cbb
Zdenek Svoboda <zsvoboda@gmail.com>
2015-06-12 15:17:32 +0200

    Chapter 7

--- book_src/src/07_deployment_recipes/executing_process.rb
+++ book_src/src/07_deployment_recipes/executing_process.rb
@@ -3,9 +3,10 @@
 require 'gooddata'
 
 GoodData.with_connection do |client|
-  project = GoodData.use('project_id')
-  process = project.deploy_process('./hello_world.rb',
-                                   type: :ruby,
-                                   name: "example_process")
-  process.execute('hello_world.rb', params: { param1: 'a', param2: 'b' })
+  GoodData.with_project('project_id') do |project|
+    process = project.deploy_process('./hello_world.rb',
+      type: :ruby,
+      name: "example_process")
+    process.execute('hello_world.rb', params: { param1: 'a', param2: 'b' })
+  end
 end
