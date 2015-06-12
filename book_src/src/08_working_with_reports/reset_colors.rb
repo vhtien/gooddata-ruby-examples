611fb82bd6351c173f86746ccb71294642d7edd8
Zdenek Svoboda <zsvoboda@gmail.com>
2015-06-12 15:37:40 +0200

    Chapter 8

--- book_src/src/08_working_with_reports/reset_colors.rb
+++ book_src/src/08_working_with_reports/reset_colors.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
 GoodData.with_connection do |client|
-  project = client.project(pid)
-
-  ids = [report_id_1, report_id_2, report_id_3]
-  ids.each do |id|
-    r = project.reports(id)
-    d = r.latest_report_definition
-    d.reset_color_mapping!
-    d.save
+  GoodData.with_project('project_id') do |project|
+    ids = [report_id_1, report_id_2, report_id_3]
+    ids.each do |id|
+      r = project.reports(id)
+      d = r.latest_report_definition
+      d.reset_color_mapping!
+      d.save
+    end
   end
 end
