c9142d5263dc2f778bc6114015a4d3487ecbdaa8
Zdenek Svoboda <zsvoboda@gmail.com>
2015-06-15 14:15:48 +0200

    Chapter 11

--- book_src/src/11_working_with_data_filters/filters_from_row_based_csv.rb
+++ book_src/src/11_working_with_data_filters/filters_from_row_based_csv.rb
@@ -2,22 +2,20 @@
 
 require 'gooddata'
 
-client = GoodData.conect('login', 'password')
-project = client.projects('project_id')
-
-# First let's grab an attribute
-attribute = project.attributes('attr.region.city')
-
-# to set up a value we need a specific label
-# if the attribute has only one label, you can easily grab it by calling #primary_label
-label = attribute.primary_label
-
-# if a label has multiple labels, you can select the correct one like this
-# label = attribute.label_by_name('City name')
-
-filters = GoodData::UserFilterBuilder::get_filters('data.csv', { 
-  :type => :filter, 
-  :labels => [{:label => label}]
-})
-
-project.add_data_permissions(filters)
+GoodData.with_connection do |c|
+  GoodData.with_project('project_id') do |project|
+    # First let's grab an attribute
+    attribute = project.attributes('attr.region.city')
+    # to set up a value we need a specific label
+    # if the attribute has only one label, you can easily grab it by calling #primary_label
+    label = attribute.primary_label
+    # if a label has multiple labels, you can select the correct one like this
+    # label = attribute.label_by_name('City name')
+    filters = GoodData::UserFilterBuilder::get_filters('data.csv', { 
+      :type => :filter, 
+      :labels => [{:label => label}]
+      })
+    project.add_data_permissions(filters)
+  end
+end
+  
--- book_src/src/11_working_with_data_filters/list_user_filters.rb
+++ book_src/src/11_working_with_data_filters/list_user_filters.rb
@@ -2,7 +2,8 @@
 
 require 'gooddata'
 
-client = GoodData.conect('login', 'password')
-project = client.projects('project_id')
-
-project.data_permissions.pmap {|f| [f.related.login, f.pretty_expression]}
+GoodData.with_connection do |c|
+  GoodData.with_project('project_id') do |project|
+    project.data_permissions.pmap {|f| [f.related.login, f.pretty_expression]}
+  end
+end
