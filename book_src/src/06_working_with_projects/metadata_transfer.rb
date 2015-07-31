d386f3ee4c3c29c67963ecac7634378545f464ff
Tomas Svarovsky <svarovsky@gooddata.com>
2015-07-31 14:11:08 -0700

    Added a transfer objects recipe

---
+++ book_src/src/06_working_with_projects/metadata_transfer.rb
@@ -0,0 +1,15 @@
+# encoding: UTF-8
+
+require 'gooddata'
+
+GoodData.with_connection do |c|
+  target_project = c.projects('target_project_id')
+
+  # Lets log into project you would like to transfer from  
+  GoodData.with_project('master_project_id') do |master_project|
+    # find objects you would like to transfer
+    # here we transfer all reports containing word "sales" in the title
+    reports = master_project.reports.select { |r| r.title =~ /title/ }
+    master_project.transfer_objects(reports, project: target_project)
+  end
+end
