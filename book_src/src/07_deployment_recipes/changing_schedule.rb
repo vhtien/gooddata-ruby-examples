254eecae0585228b7aedee9c06531894482bac71
Zdenek Svoboda <zsvoboda@gmail.com>
2015-08-19 11:21:00 +0200

    Fixing change schedule recipe

--- book_src/src/07_deployment_recipes/changing_schedule.rb
+++ book_src/src/07_deployment_recipes/changing_schedule.rb
@@ -10,27 +10,27 @@ GoodData.with_connection do |client|
   # you can change pretty much anything
 
   # executable
-  s.executable = 'entry_point.rb'
+  schedule.executable = 'entry_point.rb'
 
   # params
-  s.params
+  schedule.params
   # {
   #   "PROCESS_ID"=>"c42c1b82-7d6f-433a-b008-9cdb1d454e01",
   #   "EXECUTABLE"=>"new_main.rb",
   #   :a=>:b
   # }
-  s.set_param(:a, :c)
-  s.params
+  schedule.set_param(:a, :c)
+  schedule.params
   # {
   #   "PROCESS_ID"=>"c42c1b82-7d6f-433a-b008-9cdb1d454e01",
   #   "EXECUTABLE"=>"new_main.rb",
   #   :a=>:c
   # }
-  s.update_params({
+  schedule.update_params({
     :a => 42,
     :b => [1,2,3]
   })
-  s.params
+  schedule.params
   # {
   #   "PROCESS_ID"=>"c42c1b82-7d6f-433a-b008-9cdb1d454e01",
   #   "EXECUTABLE"=>"new_main.rb",
@@ -39,28 +39,28 @@ GoodData.with_connection do |client|
   # }
   
   # reschedule
-  s.reschedule # => 0
-  s.reschedule = 15
+  schedule.reschedule # => 0
+  schedule.reschedule = 15
   
   # name
-  s.name # => "Some Name"
-  s.name = "Better Name"
+  schedule.name # => "Some Name"
+  schedule.name = "Better Name"
 
   # enable/disable
-  s.state # => "ENABLED"
-  s.disable
-  s.state # => "DISABLED"
-  s.enable
-  s.state # => "ENABLED"
+  schedule.state # => "ENABLED"
+  schedule.disable
+  schedule.state # => "DISABLED"
+  schedule.enable
+  schedule.state # => "ENABLED"
 
   # cron expression
-  s.cron # => "1 1 1 * *"
-  s.cron = "1 1 1 1 *"
+  schedule.cron # => "1 1 1 * *"
+  schedule.cron = "1 1 1 1 *"
 
   # "run after" schedule
   after_schedule = project.schedules('some_id')
-  s.after = after_schedule
+  schedule.after = after_schedule
 
   # Do not foreget to save it
-  s.save
+  schedule.save
 end
