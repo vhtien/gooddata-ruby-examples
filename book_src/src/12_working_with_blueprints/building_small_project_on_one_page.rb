ed70de53831744d9087e95fc417d7b4413f74ea7
Tomas Svarovsky <svarovsky@gooddata.com>
2015-12-09 12:42:07 -0800

    Changing label name in report

--- book_src/src/12_working_with_blueprints/building_small_project_on_one_page.rb
+++ book_src/src/12_working_with_blueprints/building_small_project_on_one_page.rb
@@ -8,7 +8,7 @@ GoodData.with_connection('user', 'password') do |client|
     p.add_dataset('devs') do |d|
       d.add_anchor('attr.dev')
       d.add_label('label.dev_id', :reference => 'attr.dev')
-      d.add_label('label.dev_email', :reference => 'atr.dev')
+      d.add_label('label.dev_email', :reference => 'attr.dev')
     end
     p.add_dataset('commits') do |d|
       d.add_anchor('attr.commits_id')
@@ -22,23 +22,23 @@ GoodData.with_connection('user', 'password') do |client|
 
   # Load data
   commits_data = [
-    ['lines_changed', 'committed_on', 'devs'],
+    ['fact.lines_changed', 'committed_on', 'devs'],
     [1, '01/01/2014', 1],
     [3, '01/02/2014', 2],
     [5, '05/02/2014', 3]]
   project.upload(commits_data, blueprint, 'commits')
 
   devs_data = [
-    ['dev', 'email'],
+    ['label.dev_id', 'label.dev_email'],
     [1, 'tomas@gooddata.com'],
     [2, 'petr@gooddata.com'],
     [3, 'jirka@gooddata.com']]
   project.upload(devs_data, blueprint, 'devs')
 
   # create a metric
-  metric = project.facts('lines_changed').create_metric
+  metric = project.facts('fact.lines_changed').create_metric
   metric.save
-  report = project.create_report(title: 'Awesome_report', top: [metric], left: ['dev_id'])
+  report = project.create_report(title: 'Awesome_report', top: [metric], left: ['label.dev_email'])
   report.save
   ['john@example.com'].each do |email|
     p.invite(email, 'admin', "Guys checkout this report #{report.browser_uri}")
