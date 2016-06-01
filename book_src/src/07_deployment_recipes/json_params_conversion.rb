b775eb26f8dd3b26c2e58c262d5b5125698c0658
Tomas Svarovsky <svarovsky@gooddata.com>
2016-06-01 13:24:28 -0700

    Changing response in the script to return the correct values for the example

--- book_src/src/07_deployment_recipes/json_params_conversion.rb
+++ book_src/src/07_deployment_recipes/json_params_conversion.rb
@@ -11,4 +11,4 @@ JSON_DATA
 # Note that <<JSON_DATA and JSON_DATA marks the beginning and the end of the string. Once we have the JSON string defined we can use JSON libraries to convert it. Here we are using MultiJson which is part fo the Ruby SDK.
 
 params = MultiJson.load(data)
-=> {"a"=>{"b"=>"c"}} 
+=> {"param_1"=>{"deeper_key"=>"value_1"}, "param_2"=>"value_2"}
