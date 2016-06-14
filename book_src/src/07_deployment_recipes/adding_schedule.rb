5202ebce4fb003eb834b91b26c1c124ea96df8f7
Tomas Svarovsky <svarovsky@gooddata.com>
2016-06-14 10:08:50 -0700

    Added hidden params

--- book_src/src/07_deployment_recipes/adding_schedule.rb
+++ book_src/src/07_deployment_recipes/adding_schedule.rb
@@ -5,6 +5,15 @@ require 'gooddata'
 GoodData.with_connection do |client|
   GoodData.with_project('project_id') do |project|
     process = project.deploy_process('./path/to_cloud_connect_directory', name: 'Test ETL Process')
-    process.create_schedule('0 15 * * *', 'graph/my_graph.grf', params: { param1: 'a', param2: 'b' })
+    process.create_schedule('0 15 * * *', 'graph/my_graph.grf',
+      params: {
+        param_1: 'a',
+        param_2: 'b'
+      },
+      hidden_params: {
+        hidden_param_1: 'a',
+        hidden_param_2: 'b'
+      }
+    )
   end  
 end
